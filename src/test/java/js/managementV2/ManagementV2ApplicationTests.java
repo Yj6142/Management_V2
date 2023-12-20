97a42af31c7ff95ffed75108bedade3d7e60d4bf
Yj6142 <yujung6142@gmail.com>
2023-12-20 19:51:00 +0900

    Initial commit

---
+++ src/test/java/js/managementV2/ManagementV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package js.managementV2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManagementV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
