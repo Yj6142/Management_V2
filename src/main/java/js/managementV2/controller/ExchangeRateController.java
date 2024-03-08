6c2eba923cc1c9c5f99f469a1b3c07a52fa6fe80
Yj6142 <yujung6142@gmail.com>
2024-03-08 16:28:15 +0900

    Fix. 외부 환율 Open API 가져오는 코드 Service 층으로 분리

--- src/main/java/js/managementV2/controller/ExchangeRateController.java
+++ src/main/java/js/managementV2/controller/ExchangeRateController.java
@@ -1,93 +1,28 @@
 package js.managementV2.controller;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import js.managementV2.domain.ExchangeRateInfo;
+import js.managementV2.service.ExchangeRateService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 @Slf4j
 public class ExchangeRateController {
 
-    @Value("${openApi.callBackUrl}")
-    private String callBackUrl;
-
-    @Value("${openApi.authKey}")
-    private String authKey;
-
-    @Value("${openApi.data}")
-    private String data;
+    private final ExchangeRateService exchangeRateService;
 
     @GetMapping("/todayRate")
-    public ResponseEntity<List<ExchangeRateInfo>> callExchangeApi() {
+    public ResponseEntity<List<ExchangeRateInfo>> getTodayRate() {
+        List<ExchangeRateInfo> exchangeRateInfoList = exchangeRateService.callExchangeApi();
 
-        HttpURLConnection urlConnection = null;
-        InputStream stream = null;
-        List<ExchangeRateInfo> result = new ArrayList<>();
-
-        String urlStr = callBackUrl +
-                "authkey=" + authKey +
-                "&data=" + data;
-
-        try{
-            URL url = new URL(urlStr);
-
-            urlConnection = (HttpURLConnection) url.openConnection();
-            stream = getNetworkConnection(urlConnection);
-            result = readStreamToJson(stream);
-
-            if(stream != null) stream.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } finally {
-            if (urlConnection != null) {
-                urlConnection.disconnect();
-            }
-        }
-
-        return new ResponseEntity<>(result, HttpStatus.OK);
-    }
-
-    private InputStream getNetworkConnection(HttpURLConnection urlConnection) throws IOException {
-        urlConnection.setConnectTimeout(3000);
-        urlConnection.setReadTimeout(3000);
-        urlConnection.setRequestMethod("GET");
-        urlConnection.setDoInput(true);
-
-        if (urlConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-            throw new IOException("HTTP error code :" + urlConnection.getResponseCode());
-        }
-
-        return urlConnection.getInputStream();
-    }
-
-    private List<ExchangeRateInfo> readStreamToJson(InputStream stream)throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
-        ObjectMapper objectMapper = new ObjectMapper();
-        List<ExchangeRateInfo> result = new ArrayList<>();
-
-        String readLine;
-
-        while ((readLine = br.readLine()) != null) {
-            result = Arrays.asList(objectMapper.readValue(readLine, ExchangeRateInfo[].class));
-        }
-
-        br.close();
-        return result;
+        log.info("exchangeInfoList = {}", exchangeRateInfoList);
+        return new ResponseEntity<>(exchangeRateInfoList, HttpStatus.OK);
     }
 }
---
+++ src/main/java/js/managementV2/service/ExchangeRateService.java
@@ -0,0 +1,92 @@
+package js.managementV2.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import js.managementV2.domain.ExchangeRateInfo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class ExchangeRateService {
+
+    private final String callBackUrl;
+    private final String authKey;
+    private final String data;
+
+    public ExchangeRateService(@Value("${openApi.callBackUrl}") String callBackUrl,
+                               @Value("${openApi.authKey}") String authKey,
+                               @Value("${openApi.data}") String data){
+        this.callBackUrl = callBackUrl;
+        this.authKey = authKey;
+        this.data = data;
+    }
+
+    public List<ExchangeRateInfo> callExchangeApi() {
+
+        HttpURLConnection urlConnection = null;
+        InputStream stream = null;
+        List<ExchangeRateInfo> result = new ArrayList<>();
+
+        String urlStr = callBackUrl +
+                "authkey=" + authKey +
+                "&data=" + data;
+
+        try{
+            URL url = new URL(urlStr);
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            stream = getNetworkConnection(urlConnection);
+            result = readStreamToJson(stream);
+
+            if(stream != null) stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+        }
+
+        return result;
+    }
+
+    private InputStream getNetworkConnection(HttpURLConnection urlConnection) throws IOException {
+        urlConnection.setConnectTimeout(3000);
+        urlConnection.setReadTimeout(3000);
+        urlConnection.setRequestMethod("GET");
+        urlConnection.setDoInput(true);
+
+        if (urlConnection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+            throw new IOException("HTTP error code :" + urlConnection.getResponseCode());
+        }
+
+        return urlConnection.getInputStream();
+    }
+
+    private List<ExchangeRateInfo> readStreamToJson(InputStream stream)throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8));
+        StringBuilder jsonData = new StringBuilder();
+        String line;
+
+        while ((line = br.readLine()) != null) {
+            jsonData.append(line);
+        }
+
+        br.close();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        return Arrays.asList(objectMapper.readValue(jsonData.toString(), ExchangeRateInfo[].class));
+    }
+
+}
