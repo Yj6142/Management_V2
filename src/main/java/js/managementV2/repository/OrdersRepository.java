06566157b737cd9cb838e1af6fbb444a6c668b61
Yj6142 <yujung6142@gmail.com>
2024-03-12 18:09:44 +0900

    Feat. OrderList 및 TodayRate 기능 구현

--- src/main/java/js/managementV2/repository/OrdersRepository.java
+++ src/main/java/js/managementV2/repository/OrdersRepository.java
@@ -8,8 +8,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.time.LocalDate;
-import java.util.Date;
 import java.util.List;
 
 public interface OrdersRepository extends JpaRepository<Orders, Long> {
